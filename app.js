9493f5cf6d5b0c0167e0fe98aad882f89134d2a6
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-03-23 19:18:35 +0300

    added the css

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const express = require("express");
+const { redirect } = require("express/lib/response");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+let tasks = ["First task"];
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
+  res.sendFile(__dirname);
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItem: tasks });
 });
 
 app.post("/", function (req, res) {
-  var task = req.body.newItem;
-  console.log(task);
+  let newTask = req.body.newItem;
+  tasks.push(newTask);
+  console.log(newTask);
+  res.redirect("/");
 });
