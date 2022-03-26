bf52ea99d3b0ee75cf797c2d5e3f9024981a905c
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-03-26 22:22:41 +0300

    added the a work list route.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const { redirect } = require("express/lib/response");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -7,10 +8,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 let tasks = ["First task"];
+let workTasks = ["Attend work"];
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+// this is general list, access by the / (root) route
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -19,16 +23,30 @@ app.get("/", function (req, res) {
     day: "numeric",
     month: "long",
   };
-  res.sendFile(__dirname);
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItem: tasks });
+  res.render("list", { listTitle: day, newListItem: tasks });
 });
+
+// after entering the task, the new task will be added to the array and then
+// the server will redirect and displayb the new list
 
 app.post("/", function (req, res) {
   let newTask = req.body.newItem;
   tasks.push(newTask);
   console.log(newTask);
   res.redirect("/");
+});
+
+// this is for the work list, access by the /work route
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work list", newListItem: workTasks });
+});
+
+app.post("/work", function (req, res) {
+  let newWorkTask = req.body.newItem;
+  workTasks.push(newWorkTask);
+  res.redirect("/work");
 });
