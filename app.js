9738deca07a166adcb29f099bd21e6bb0afac6b2
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-04-07 23:52:28 +0300

    connected with mongoDB

--- app.js
+++ app.js
@@ -2,44 +2,29 @@ const express = require("express");
 const app = express();
 const date = require(__dirname + "/date.js");
 const day = date.getDate();
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const tasks = ["First task"];
-const workTasks = ["Attend work"];
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/todolistDB");
+}
+
+const itemsSchema = mongoose.Schema({
+  name: String,
+});
+
+const Items = mongoose.model("Item", itemsSchema);
+
+const itemsArray = ["buy groceries", "Cook food", "clean dishes"];
+
+app.get("/", function (req, res) {
+  res.render("list", { listTitle: day, newListItem: itemsArray });
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
-// this is general list, access by the / (root) route
-
-app.get("/", function (req, res) {
-  res.render("list", { listTitle: day, newListItem: tasks });
-});
-
-// after entering the task, the new task will be added to the array and then
-// the server will redirect and displayb the new list
-
-app.post("/", function (req, res) {
-  const newTask = req.body.newItem;
-  if (req.body.list === "Work List") {
-    //if the route is "work" then add the new task and go to the work route again
-    workTasks.push(newTask);
-    res.redirect("/work");
-  } else {
-    // else if the route is the home route then add to the list and go to the home route again
-    tasks.push(newTask);
-    res.redirect("/");
-  }
-});
-
-// this is for the work list, access by the /work route
-
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItem: workTasks });
-});
-
-app.post("/work", function (req, res) {});
