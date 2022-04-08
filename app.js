9b6f3f49e1fe8624e41306522930fca209fe4184
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-04-09 01:42:27 +0300

    can now delete items when clicking the checkbox

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { name } = require("ejs");
 const express = require("express");
 const app = express();
 const date = require(__dirname + "/date.js");
@@ -17,12 +18,39 @@ const itemsSchema = mongoose.Schema({
   name: String,
 });
 
-const Items = mongoose.model("Item", itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
-const itemsArray = ["buy groceries", "Cook food", "clean dishes"];
+defaultItems = [
+  { name: "buy groceries" },
+  { name: "Cook food" },
+  { name: "Clean dishes" },
+];
+
+app.post("/delete", function (req, res) {
+  Item.deleteOne({ _id: req.body.checkbox }, function (err) {
+    if (err) console.log("failed to delete");
+    else res.redirect("/");
+  });
+  console.log(req.body.checkbox);
+});
 
 app.get("/", function (req, res) {
-  res.render("list", { listTitle: day, newListItem: itemsArray });
+  Item.find({}, function (err, results) {
+    // if the DB is empty then add some values
+    if (results.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) console.log("insertion failed");
+      });
+    }
+    res.render("list", { listTitle: day, newListItem: results });
+  });
+});
+
+app.post("/", function (req, res) {
+  Item.insertMany({ name: req.body.newItem }, function (err) {
+    if (err) console.log("insertion failed");
+    else res.redirect("/");
+  });
 });
 
 app.listen(3000, function () {
